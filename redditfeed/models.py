edf9aa61b17a04cf37db0f15dd8ebe3fbb904a50
Artur <52126850+ArturAyvazyan@users.noreply.github.com>
2020-12-04 00:03:13 +0300

    Update models.py

--- redditfeed/models.py
+++ redditfeed/models.py
@@ -7,8 +7,3 @@ class News(models.Model):
     text = models.TextField(max_length = 500)
     image = models.ImageField(verbose_name='Изображение')
     date = models.DateTimeField(auto_now_add=True, blank=True)
-
-    #class Meta:
-    #    ordering = ['id']
-    #video =  models.URLField(max_length=128, db_index=True, unique=True, blank=True)
-
